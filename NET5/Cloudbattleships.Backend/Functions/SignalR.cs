77f75b1a789b8eedb079830130852b199da45d83
Daniel Sandberg <daniel.sandberg@elicit.se>
2021-07-26 20:41:07 +0200

    Added Blazor UI Added Model Added Backend Added SignalR nugets

---
+++ NET5/Cloudbattleships.Backend/Functions/SignalR.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+
+namespace Cloudbattleships.Backend.Functions
+{
+    public static class SignaR
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate
+        (
+            [HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "cloudbattleships")] SignalRConnectionInfo connectionInfo
+        ) => connectionInfo;
+    }
+}
