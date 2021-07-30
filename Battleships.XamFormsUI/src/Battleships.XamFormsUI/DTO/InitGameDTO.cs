0cd154503cf14d120d582908834ab4915cad0e73
Daniel Sandberg <daniel.sandberg@elicit.se>
2021-07-30 14:07:45 +0200

    Removed old version

--- Battleships.XamFormsUI/src/Battleships.XamFormsUI/DTO/InitGameDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Battleships.XamFormsUI
-{
-    public class InitGameDTO
-    {
-        public string ClientId { get; set; }
-        public string User { get; set; }
-    }
-}
--- Battleships.XamFormsUI/src/Battleships.XamFormsUI/DTO/UpdateGameDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿using Battleships.Model.DTO;
-
-namespace Battleships.XamFormsUI
-{
-    public class UpdateGameDTO
-    {
-        public GameDTO Game { get; set; }
-    }
-}
